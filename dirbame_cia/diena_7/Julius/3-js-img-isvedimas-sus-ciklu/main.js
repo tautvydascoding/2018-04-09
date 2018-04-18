1a48f38a9d3ff7be84aafdb5f30205aff7e5626b
Julius <julius7070@gmail.com>
2018-04-18 09:46:41 +0300

    Komandine uzduotis

---
+++ dirbame_cia/diena_7/Julius/3-js-img-isvedimas-sus-ciklu/main.js
@@ -0,0 +1,28 @@
+console.log( "labas as cia"
+// 2.2 UZDUOTIS
+// paleisti f-ja "printX(xx)" 12 kartu
+
+//nuotrauka - String
+var nuotrauka = " <img src='writing.jpg' alt='piestukas' width='25%'> ");
+for (var i = 0; i < 12; i++) {
+   printX( nuotrauka );
+}
+// 4 UZDUOTIS
+// A) sukurti f-jas piestiEilute(x); spausdintiStulpeli(x);
+// "x" skaicius, kuris nurodo kokio ilgumo eilkute/stulpelis piesti
+//
+// B) nupiesti tuscia lentele paleidiznat f-jas (  antrastine eilute "vardas |  pavarde | amzius" - teks isvesti be f-jos)
+
+// ----------------------------
+// vardas |  pavarde | amzius
+// ----------------------------
+// |
+// |
+
+
+function piestiEilute( ilgis ){
+  var eilkute = "";
+  for (var i = 0; i ,< ilgis; i++) {
+    eilute
+  }
+}
