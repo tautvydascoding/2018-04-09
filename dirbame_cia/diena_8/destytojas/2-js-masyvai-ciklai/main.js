96ae0b0566d018fbad0050820c6fe30012f25a84
grajauskas <e.grajauskas@gmail.com>
2018-04-19 10:33:46 +0300

    8 diena

--- dirbame_cia/diena_8/destytojas/2-js-masyvai-ciklai/main.js
+++ dirbame_cia/diena_8/destytojas/2-js-masyvai-ciklai/main.js
@@ -13,30 +13,46 @@ console.log( "uzpildytas masyvas: ",   masyvas );
 
 // 5.1  sukurti masyva naudojant FOR (50 ilgumo) ir uzpildyti visa skaiciumi 1;
 
-var m = [];
+var mas = [];
 for ( var i=0; i < 50; i++) {
-    m[i] = 1;
+    mas[i] = 1;
 }
-console.log(  m );
+console.log(  mas );
 
 // UZDUOTIS
 // pasikartoti if: patikrinti masyvo pradzia.
 // jeigu pradzioje irasytas "1" - isvesti "vienas",
 //  priesingu atveju - isvesti "ne vienas"
 
-if ( m[0] ===  1 ) {
-    console.log( "vienas ");
-} else {
-    console.log( "ne vienas ");
-}
-
-
-
+// if ( m[0] ===  1 ) {
+//     console.log( "vienas ");
+// } else {
+//     console.log( "ne vienas ");
+// }
 
 // 5.2: kas antra ^ masyvo elementa pakeisti i "3"
 // [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, ....
+
+// for (var i = 0; i < mas.length; i++) {
+//     // jeigu liekana nera lygi "0"
+//     var liekana = i % 2 ;
+//     if (  liekana != 0 ) {
+//         mas[i] = 3;
+//     }
+// }
+// console.log( "kas antras 3: ",  mas );
+// arba
+for (var i = 1; i < 50; i = i + 2 ) {  // i = i + 2   =>  i += 2;
+    mas[i] = 3;
+}
+console.log( "kas antras 3: ",  mas );
+
 // 5.3: kas 5-ta ^ masyvo elementa pakeisti i "9"
 // [1, 3, 1, 3, 9, 3, 1, 3, 1, 9, ....
-
+for (var i = 0; i < mas.length ; i++) {
+    i += 4;
+    mas[i] = 9;
+}
+console.log( "kas penktas 9: ",  mas );
 
 //=============bootstrap---sm-md-lg-xl (30-60m)==========
---
+++ dirbame_cia/diena_9/destytojas/1-js-DEBUGER/main.js
@@ -0,0 +1,16 @@
+console.log( "labas as cia" );
+
+// Uzuoduotis
+// PATIKRINTI ar masyve yra ieskomas skaicius
+var ieskomas = 1;
+//          i= 0   1  2  3  4    5   6    7  8
+var masyvas = [5, 10, 1, 6, 89, 100, 55, 20, 30];
+
+//masyvas.length - skaicius kiek masyve yra stalciu
+for (var i = 0; i < masyvas.length; i++) {
+    if ( masyvas[i] === 55  ) {
+        console.log(" Radau. Stalciuje: ", i  );
+    }
+}
+
+// visual studio code (Microsoft) - Nemokama
