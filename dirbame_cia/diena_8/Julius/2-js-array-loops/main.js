c6ecddd3f1efa808fba762ada54513e546841b14
grajauskas <e.grajauskas@gmail.com>
2018-04-20 12:15:26 +0300

    dina 10.1

---
+++ dirbame_cia/diena_8/Julius/2-js-array-loops/main.js
@@ -0,0 +1,46 @@
+console.log( "labas as cia" );
+// ]4. sukurti masyva (50 ilgumo) naudojant FOR ir uzpildyti visa skaiciais "0" (kiekvienas stalcius turi but lygus "0");
+
+var masyvas = []; // tuscio masyvo kurimas
+
+for( var i = 0; i < 50; i++) { //uzpildome masyva nuliais
+  masyvas[i] = 0;
+}
+console.log( "uzpildytas masyvas", masyvas );
+
+
+// 5.1  sukurti masyva naudojant WHILE (50 ilgumo) ir uzpildyti visa skaiciumi 1;
+
+var m = [];
+for ( var i=0; i < 50; i++){
+  m[i] = 1
+}
+console.log( m );
+
+//--------------------------//
+
+if ( m[0] === 1 ) {
+  console.log( "vienas" );
+} else {
+    console.log( "ne vienas" );
+}
+
+// 5.2: kas antra ^ masyvo elementa pakeisti i "3"
+// [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, ....
+
+for (var i = 0; i < mas.length; i++) {
+  if (i % 2 === 0){
+    mas[i] = 3;
+  }
+}
+console.log( "kas antras 3: ", mas);
+
+
+// 5.3: kas 5-ta ^ masyvo elementa pakeisti i "9"
+// [1, 3, 1, 3, 9, 3, 1, 3, 1, 9, ....
+
+for (var i = 4; i < mas.length ; i++){
+  i += 4;
+  mas[]
+}
+//=============bootstrap---sm-md-lg-xl (30-60m)==========
