f6d84864ad2d8c671a8c6a17fd820b4a72c6a2cb
Robertas Rudys <robertas.rudys@gmail.com>
2018-04-20 17:59:43 +0300

    Diena 10, php

---
+++ dirbame_cia/diena_10/robertas/2-php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Title</title>
+  <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+  <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.css">
+  <link rel="stylesheet" href="css/master.css">
+</head>
+
+<body>
+  <script src="libs/jquery-3.2.1.min.js"></script>
+  <?php echo "Labas." ?>
+  <!-- <?php echo "<script src='main.js'></script>" ?> -->
+</body>
+
+</html>
