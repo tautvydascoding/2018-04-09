4664f99644e296f302475c88b293178a38e0e18e
Julius <julius7070@gmail.com>
2018-04-23 15:11:29 +0300

    no message

--- dirbame_cia/diena_11/Julius/tuscias-CSS-projektas2/tuscias-CSS-projektas/main.js
+++ dirbame_cia/diena_11/Julius/tuscias-CSS-projektas2/tuscias-CSS-projektas/main.js
@@ -9,7 +9,14 @@ var x = $( 'h1'). text;
 var y =
 
 
-.console.log( x );
+console.log( x );
 console.log( y);
 
-$('input').on( 'br')
+$('input').on( 'blur', )
+
+
+//------------//
+
+//--pakeisti paskutini h1 tekstas--//
+$( 'h1'). eg(-1). rext('Pakeistas');
+$( 'h1').html('<b>.Pakeistas');
