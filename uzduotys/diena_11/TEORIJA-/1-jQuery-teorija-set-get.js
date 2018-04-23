0142d0d166dd9fb4b365ac7e43d09d0ec89b24fc
grajauskas <e.grajauskas@gmail.com>
2018-04-23 15:24:28 +0300

    11.4

--- uzduotys/diena_11/TEORIJA-/1-jQuery-teorija-set-get.js
+++ uzduotys/diena_11/TEORIJA-/1-jQuery-teorija-set-get.js
@@ -20,7 +20,7 @@ console.log("[0]: " ,  elm2);
 console.log( elm.text());         // WORKS !!!
 // console.log( elm2[0].text();    // ERROR: [0] return the DOM element
 // NOTE: $(...)[0] will return the DOM element. If you want to use a jquery function, it has to be on a jquery selector object. If you want to get the jquery selector for a specific index, use the eq function:
-// NOTE:  eq( -2 ) - nuo galo
+// NOTE:  eq( -2 ) - nuo galo antras
 
 
 
@@ -44,38 +44,9 @@ $("h1").html("<button> Registruotis </button>");
 // $('form input').attr('class', 'bg-info');
 //---JS ---
 
-//document.getElementsByTagName("input")[0].value = "Tomas";
-//document.getElementsByTagName("input")[0].setAttribute('value','Tomas');
-//document.myform.vardas.value = 'Tomas';
+// document.getElementsByTagName("input")[0].value = "Tomas";
+// document.getElementsByTagName("input")[0].setAttribute('value','Tomas');
+// document.myform.vardas.value = 'Tomas';
 //                 <form name="myform">
 //                         <input class="aa" name="vardas" type="text" value="" >
 //                     </form>
-
-//=====================================
-//  objekto (Node) arba teksto - idejimas i HTML
-//=====================================
-// append(x, [y], [z], ... ) - Inserts content at the end of the selected elements (viduje, gale)
-
-// prepend() - Inserts content at the beginning of the selected elements (viduje, pradzioje)
-// after() - Inserts content after the selected elements (isoreje, po elemento)
-// before() - Inserts content before the selected elements (isoreja, pries elementa)
-// insertBefore();    // The .before() and .insertBefore() methods perform the same task.
-// appendTo() - i kur perkelti si elementa
-
-var elementasH1 =   $("h1"),
-    appendText  = "<b>appended text</b>",
-    prependText = "<b>prepend text</b>",
-    afterText   = "<b>after text</b>",
-    beforeText  = "<b>before text</b>";
-
-//elementasH1.append( appendText);
-//elementasH1.prepend( prependText);
-//elementasH1.after( afterText);
-//elementasH1.before( beforeText);
-//elementasH1.appendTo( "html");  // h1 perkels i html pabaiga
-
-// ========tricks=====================
-// perkels (nekopijuos)  elementa i kita  vieta
-$( "body" ).append( $( "h1" ) );
-
-// Important: If there is more than one target element, however, cloned copies of the inserted element will be created for each target except for the last one.
