2d68b86014b5f535615669ede79e86a5a03ebd87
Julius <julius7070@gmail.com>
2018-04-23 14:34:53 +0300

    ...

---
+++ dirbame_cia/diena_11/Julius/tuscias-CSS-projektas2/tuscias-CSS-projektas/main.js
@@ -0,0 +1,15 @@
+console.log( "labas as cia" );
+
+
+$('h1').css('color', 'green');
+
+var x = $ = $(h1). css('color', 'green');
+
+var x = $( 'h1'). text;
+var y =
+
+
+.console.log( x );
+console.log( y);
+
+$('input').on( 'br')
