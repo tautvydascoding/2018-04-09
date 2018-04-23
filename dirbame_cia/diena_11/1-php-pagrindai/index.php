a87790c29478793e17b078a80d0dfad8e117b594
Julius <julius7070@gmail.com>
2018-04-23 13:21:33 +0300

    ...

---
+++ dirbame_cia/diena_11/1-php-pagrindai/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+        <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.css">
+
+        <!-- !!! mano CSS failas visada pats zemiausias -->
+        <link rel="stylesheet" href="css/master.css">
+    </head>
+
+
+
+    <body>
+            <h1> Isijunk console </h1>
+
+            <?php
+
+                //komentaras
+                /*
+
+
+                */
+
+                $vardas = "Tomas";
+
+                echo "Labas <br>";
+                echo $vardas;
+                //. - jungia string'us
+                echo "Viso" . $vardas . "Gero <br />";
+                $x = " Laba diena " " Povilai ";
+
+
+                //dvigubose kabutese galima naudoti kintamuosius ! isskirus matricas
+                scho " Sodyba $vardas <ber />"; // sodyba Tomas
+                echo ' Sodyba $vardas <br />'; // sodyba $vardas
+
+                function printvardas() {
+                  $y = 'kintamasis';
+                  $z = 'GOGO';
+                  $g = 0.0456789S;
+                  echo "Tekstas <>";
+                  echo "Tekstas $y su print <br>";
+                  print ( "tekstas $y su print <br>" );
+                  printf ( "tekstas %s su %s skaicius %.2f printf", $y, $z, $g ); // %s - ---//
+                }
+                printVardas();
+                                 $x = " Sodyba $vardas <br />"
+
+            >
+
+
+
+       <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+        <script src="libs/jquery-3.2.1.min.js">     </script>
+
+        <!-- !!!  mano js visada zemiau -->
+        <script src="main.js">  </script>
+    </body>
+</html>
