f15d7082047cdc843f37e5321561ba16a4ca54ca
Julius <julius7070@gmail.com>
2018-04-23 16:51:44 +0300

    kkkk

--- dirbame_cia/diena_11/Julius/tuscias-CSS-projektas20/tuscias-CSS-projektas/index.php
+++ dirbame_cia/diena_11/Julius/tuscias-CSS-projektas20/tuscias-CSS-projektas/index.php
@@ -38,44 +38,7 @@
             <form>
 
 
-            input type= ''
-
-            // UZDUOTIS 1
-            // pakeisti visus h1 tekstus i "Antraste nr: XX" ( vietoj "xx" isvesti skaiciu, kelinta tai antrate)
-
-            //  UZDUOTIS 2
-            // pakeisti visus h2 tekstus, kurie yra "section" elemente.
-            // pakeisti i "Pakeista antraste "
-            //
-
-            // UZDUOTIS 3
-            // i console atspausdinti "li" elemento, kurio klase "reklama" - teksta
-
-            // UZDUOTIS 4
-            // i console atspausdinti  visus "li" tekstus
-
-            //-------advance --------
-
-            // UZDUOTIS 5
-            // sukeisti vietomis "li" elementu "about" ir "gallery" (naujant jQuery)
-
-
-            //------ atributes--------------------------
-            // UZDUOTIS 6
-            // surasti <li classs='reklama'> ir jam ideti dar viena taributa: style='color: red'   (naujant jQuery)
-
-            // UZDUOTIS 7
-            // A. visiems "li" elementams pridete atributa: classs='nav-item '   (naujant jQuery)
-            // B. pirmam "li" elementui uzddeti dar viena klase: "active"
-
-            // UZDUOTIS 8
-            //   su jquery uzpildyti form'a uz vartotoja:
-            //  visiems "input" elementams pridete atributa: value=' xxx '   (naujant jQuery)
-            //  ivesti vardas: 'Tomas'
-            //  ivesti pastas: 'a@a.lt'
-
-
-
+      
 
        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
         <script src="libs/jquery-3.2.1.min.js">     </script>
