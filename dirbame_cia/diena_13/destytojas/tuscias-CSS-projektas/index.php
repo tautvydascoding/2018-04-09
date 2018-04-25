40a2197f9c625ab0d907151bb6fb28940217432c
grajauskas <e.grajauskas@gmail.com>
2018-04-25 17:08:01 +0300

    12.4

---
+++ dirbame_cia/diena_13/destytojas/tuscias-CSS-projektas/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+        <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.css">
+
+        <!-- !!! mano CSS failas visada pats zemiausias -->
+        <link rel="stylesheet" href="css/master.css">
+    </head>
+
+
+
+    <body>
+
+        <div class="container-fluid">
+            <h1> Isijunk   </h1>
+
+        </div>
+
+        <section>
+            <h2>  Antraste</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
+        
+        <aside>
+            <h2>  2 Antraste</h2>
+            <p> 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </aside>
+
+
+
+       <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+        <script src="libs/jquery-3.2.1.min.js">     </script>
+
+        <!-- !!!  mano js visada zemiau -->
+        <script src="main.js">  </script>
+    </body>
+</html>
