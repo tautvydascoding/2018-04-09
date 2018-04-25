a6919e8afd6b756016537d3c19cc66d56363a583
Tautvydas <arttgd@gmail.com>
2018-04-25 08:28:46 +0300

    14

---
+++ uzduotys/diena_14/UZDUOTYS/uzduotys-2-PHP--include-variable-and-foreach.php
@@ -0,0 +1,16 @@
+// UZDUOTIS
+// 1. suskurti faila:  straipsnis.php
+// 1.1 straipsnis.php faile sukurti 2 kintamuosius ir masyva(antraste, aprasymas, data):
+// $vardas, $pavarde
+// masyvas:  'BMW vel preleke saligatviu',   'Lorem lorem....', '2010-12-01'
+
+// 2. index.php faile  ideti:
+// include( './straipsnis.php');
+
+// 2.1 index.php faile sukurti   'container' 'main' 'article'
+// 2.2 sukurti <div> ir jo viduje isvesti su echo $vardas, $pavarde
+// 2.3 'article' videje sukurti:
+// 2.3.1    sukurti 'h2' ir jo viduje isvesti masyve esancia  (antraste)
+// 2.3.2    sukurti 'paragrafa' ir jo viduje isvesti masyve esancia    (aprasymas)
+// 2.3.3    sukurti '<div>' ir jo viduje isvesti masyve esancia  (data)
+// 2.3.4     sukurti '<div>' ir jo viduje isvesti masyve esancia  (data)
