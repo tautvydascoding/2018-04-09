06a7fe0879cea8f359f11505b6d5dc05cc6ee61d
Julius <julius7070@gmail.com>
2018-04-26 12:29:08 +0300

    mokausi parsisiusti

--- dirbame_cia/diena_14/destytojas/2-php-blogas-straipsniai/index.php
+++ dirbame_cia/diena_14/destytojas/2-php-blogas-straipsniai/index.php
@@ -15,6 +15,14 @@
     <body>
             <h1> Isijunk   </h1>
 
+  <?php
+
+   $
+
+
+
+  ?>
+
 
 
        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
