9ee83f5c783b279d74981d324236615020814f3a
grajauskas <e.grajauskas@gmail.com>
2018-04-26 12:32:14 +0300

    14.4

--- dirbame_cia/diena_14/destytojas/2-php-blogas-straipsniai/index.php
+++ dirbame_cia/diena_14/destytojas/2-php-blogas-straipsniai/index.php
@@ -14,7 +14,32 @@
 
     <body>
             <h1> Isijunk   </h1>
+<?php
 
+ $vardas = 'Tomas';
+ $pavarde = 'Tomauskis';
+
+ $straipsnis = [];
+ $straipsnis["antraste"] = 'Brangsta butai, Kaunas pleciasi';
+ $straipsnis["aprasymas"] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+ $straipsnis["data"] = '2018/04/26';
+
+include('straipsnis.php');
+include('straipsnis.php');
+include('straipsnis.php');
+include('straipsnis.php');
+
+ // echo  $straipsnis['aprasymas'];
+
+ // foreach ($straipsnis as   $vienoStalciausTurinys) {
+ //     echo  $vienoStalciausTurinys . "<br>";
+ // }
+
+
+
+
+
+ ?>
 
 
        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
