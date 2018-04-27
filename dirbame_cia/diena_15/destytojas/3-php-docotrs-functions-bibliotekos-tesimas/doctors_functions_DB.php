bca9f3c5d90f14f3b6fe8371d44093348659cbee
grajauskas <e.grajauskas@gmail.com>
2018-04-27 14:45:04 +0300

    14.4

---
+++ dirbame_cia/diena_15/destytojas/3-php-docotrs-functions-bibliotekos-tesimas/doctors_functions_DB.php
@@ -0,0 +1,78 @@
+<?php
+
+
+// 2018-04-26
+// Tautvydas
+
+//    kintamuju vardai , reiksme
+define( 'HOST', 'localhost' );
+define( 'MYSQL_VARTOTOJAS', 'root' );  // root
+define( 'MYSQL_SLAPTAZODIS', 'root' );
+define( 'DB_VARDAS', 'hospital4' );
+
+// jungiames pr. DB  'hospital4'
+ $connection = mysqli_connect( HOST, MYSQL_VARTOTOJAS, MYSQL_SLAPTAZODIS, DB_VARDAS);   // !!! jeigu pas jus pakeistas MYSQL PORT pvz:8889, reik nurodyt
+
+ // f-ja
+ function getPrisijungimas() {
+     global $connection; // sioje f-joje naudosime blobalu kintamaji
+     if ($connection) {
+         //echo "Prisijungete sekmingai <br>";
+         return $connection;
+     } else {
+          echo "EROROR  nepavyko prisijungti pr. DB <br>" . mysqli_connect_error();
+     }
+ }
+getPrisijungimas();
+
+//
+function getDoctor( $nr ) {
+    $mano_sql = "SELECT * FROM doctors
+                          WHERE id='$nr';
+                ";
+    //                             irodymas            SQL komandos
+    $rezultatai = mysqli_query(  getPrisijungimas()  , $mano_sql);
+    // var_dump($rezultatai); // test
+
+    // mysqli_num_rows - suskaiciuoja kiek radai gydytoju
+    if (mysqli_num_rows($rezultatai)  > 0) {
+        // mysqli_fetch_assoc  - apdoroje duomenis
+        // is paduoto MYSQL objekto paima sekancia eilute ir pavercia i masyva
+        $rezultatai = mysqli_fetch_assoc($rezultatai);
+         return $rezultatai;
+    }  else {
+        echo "ERROR: gydytojo $nr nera <br />";
+    }
+}
+// $gydytojas = getDoctor(3);
+// print_r(   $gydytojas  );
+
+// gydytojo idejimmas i DB
+function createDoctorMINI($vardas, $pavardas) {
+    $manoSQL = "INSERT INTO doctors
+                       VALUES ('', '$vardas', '$pavardas');
+                ";
+    mysqli_query(  getPrisijungimas() , $manoSQL);
+}
+function createDoctor($vardas, $pavardas) {
+    $vardas = htmlspecialchars( $vardas, ENT_QUOTES, 'UTF-8' );
+    $pavardas = htmlspecialchars( $pavardas, ENT_QUOTES, 'UTF-8' );
+
+    $manoSQL = "INSERT INTO doctors
+                       VALUES ('', '$vardas', '$pavardas');
+                ";
+    $x = mysqli_query(  getPrisijungimas() , $manoSQL);
+    if ($x) {
+        echo "Pavyko sukurti gydytoja <br>";
+    }
+}
+// createDoctor('Petars', ' Petrauskis\"s  ' );
+// echo "Petrauskis&quot;s";
+
+
+// atsijungiamm nuo DB, ideti i HTML failo apacia
+// mysqli_close( $connection );
+
+
+
+//
