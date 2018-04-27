04cd807885dea8fc4a4ced0f57945eb6400ae967
Robertas Rudys <robertas.rudys@gmail.com>
2018-04-27 22:45:23 +0300

    Diena 15

---
+++ dirbame_cia/diena_15/robertas/1-form/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+        <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.css">
+
+        <!-- !!! mano CSS failas visada pats zemiausias -->
+        <link rel="stylesheet" href="css/master.css">
+    </head>
+
+
+
+    <body>
+      <form action="register.php" method="get">
+        <input type="text" name="name" value="">
+        <input type="text" name="lastname" value="">
+        <input type="tel" name="telephone" value="">
+        <button type="submit" name="button" value=""> Register </button>
+      </form>
+
+       <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+        <script src="libs/jquery-3.2.1.min.js">     </script>
+
+        <!-- !!!  mano js visada zemiau -->
+        <script src="main.js">  </script>
+    </body>
+</html>
