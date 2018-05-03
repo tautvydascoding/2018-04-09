8dd47ae33a03ec0fabcac080a6a8ec0694eeb2a6
Kriste Sia <kr1stute@yahoo.com>
2018-05-03 22:52:25 +0300

    vakariniai darbai.

---
+++ dirbame_cia/PROJEKTAI/Kriste/projektas1/shop-all.php
@@ -0,0 +1,67 @@
+<?php
+include('header.php');
+ ?>
+
+
+
+<!-- start 1 containeris -->
+<div class="container-fluid">
+  <!-- start 1 row -->
+  <div class="row aukstis-350 d-flex justify-content-center mt-3">
+
+    <!-- start 1 column -->
+    <a href="rings.php" class="col-md-4 m-2 rings"></a>
+    <!-- end 2 column -->
+
+    <!-- start 2 column -->
+    <a href="earings.php" class="col-md-3 m-2 earings"></a>
+    <!-- end 2 column -->
+
+  <!-- end 1 row -->
+  </div>
+<!-- end 1 containeris -->
+</div>
+
+
+<!-- start 2 containeris -->
+<div class="container-fluid">
+  <!-- start 1 row -->
+  <div class="row aukstis-250 d-flex justify-content-center mb-3">
+
+    <!-- start 1 column -->
+    <a href="keychains.php" class="col-md-3 m-2 keychains"></a>
+    <!-- end 1 column -->
+
+    <!-- start 2 column -->
+    <a href="bracelets.php" class="col-md-4 m-2 bracelets">
+    </a>
+    <!-- end 2 column -->
+
+  <!-- end 1 row -->
+  </div>
+<!-- end 2 containeris -->
+</div>
+
+
+
+
+
+ <!-- start Footer container -->
+     <div class="container-fluid aukstis-100">
+       <!-- start 1 row -->
+       <div class="row footer">
+
+         <!-- start 1 column -->
+         <div class="col-md aukstis-100 d-flex align-items-end justify-content-end teises">
+           <i class="fa fa-copyright"> 2018, Kriste Sia </i>
+         </div>
+         <!-- end 1 column -->
+
+       </div>
+       <!-- end 1 row -->
+     </div>
+   <!-- end Footer container -->
+
+ <?php
+ include('footer.php');
+  ?>
