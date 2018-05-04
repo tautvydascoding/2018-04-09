c965a5192abf9261d78847b67075056f21c345ef
grajauskas <e.grajauskas@gmail.com>
2018-05-04 10:34:32 +0300

    16.2

--- uzduotys/diena_18/UZDUOTYS/uzduotys-99-php-abstract---+-+.php
+++ uzduotys/diena_18/UZDUOTYS/uzduotys-99-php-abstract---+-+.php
@@ -1,15 +1,17 @@
 
     // uzduotis:
-    // 1) sukurti interface "Edit" : kuri turi funkcijas: (kurios nera aprasytos (privalomos)!
-        // 0) Ar gali tureti kintamuosius????
+    // 1) sukurti interface "Edit" : kuris turi funkcijas:
         // changeName($a)
         // isLogin()
         // getName()
+    // 1.1) Ar interface'ai gali tureti kintamuosius????
     // 2) sukurti (abstrakcia) klase "User" :
-        // 0) abstrakti kalse - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
         // A) kuri turi neprivalomus kintamuosius:
             // name, password , createdDate, lastLogin
         // B) privaloma funkcija "Loguot()"
         // C) neprivaloma funkcija clearlastLoginTime()
     // 3) sukurti klase Admin :
         // A)  kuri panaudoja Edit ir User ~klases (implementuoti ir paveldeti)
+
+
+    NOTE: abstrakti kalse - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
